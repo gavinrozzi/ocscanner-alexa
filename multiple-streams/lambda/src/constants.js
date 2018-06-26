7857f492bb09db3931365f6d660449ae7280eca9
Gavin Rozzi <gr@gavinrozzi.com>
2018-06-25 20:57:53 -0400

    Update skill

--- multiple-streams/lambda/src/constants.js
+++ multiple-streams/lambda/src/constants.js
@@ -2,16 +2,16 @@
 
 exports.skill = {
   appId: '',
-  dynamoDBTableName: 'Audio-Player-Multi-Stream',
+  dynamoDBTableName: 'ocscanner-alexa',
 };
 
 exports.audioData = [
   {
-    title: 'Episode 139',
-    url: 'https://feeds.soundcloud.com/stream/274166909-amazon-web-services-306355661-aws-podcast-episode-139.mp3',
+    title: 'Northern Ocean County Scanner Feed',
+    url: 'https://ocscanner.news/livefeedmainssl',
   },
   {
-    title: 'Episode 140',
-    url: 'https://feeds.soundcloud.com/stream/275202399-amazon-web-services-306355661-amazon-web-services.mp3',
+    title: 'Southern Ocean County Scanner Feed',
+    url: 'https://ocscanner.news/livefeedmainssl',
   },
 ];
